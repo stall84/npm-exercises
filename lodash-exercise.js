a58cb8b80366855c63a6d71e5c25895aef160bba
Michael Stallings <stall84@gmail.com>
2020-04-02 21:25:02 -0400

    finished lodash exercises

---
+++ lodash-exercise.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+
+let arr = [4,8,2,1];
+
+let shuffArr = _.shuffle(arr);
+
+console.log(shuffArr);
+
+
+let arr2 = [13,33,85,9];
+
+let sumArr2 = _.sum(arr2);
+
+console.log(sumArr2);
+
+
+let randomNum = _.random(1.5,3.8);
+
+console.log(randomNum);
